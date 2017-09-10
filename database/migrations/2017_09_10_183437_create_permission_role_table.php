121afe20d087925cb2bec093034d35e474e07328
RobinPerido <robin.sandstrom@perido.se>
2017-09-10 18:50:14 +0000

    Creating basic permissions migrations

---
+++ database/migrations/2017_09_10_183437_create_permission_role_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissionRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('permission_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->foreign('permission_id')->references('id')->on('permissions');
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+
+        DB::table('permission_role')->insert([
+            [
+                'permission_id' => '1',
+                'role_id' => '1',
+            ],
+            [
+                'permission_id' => '1',
+                'role_id' => '2',
+            ],
+            [
+                'permission_id' => '1',
+                'role_id' => '3',
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_role');
+    }
+}
